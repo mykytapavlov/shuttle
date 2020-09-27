645abd215933802a92000a98cdd846b7338158c0
mykytapavlov <mykytapavlov@users.noreply.github.com>
2020-09-27 15:13:49 +0300

    #2: Creates FastAPI template project with a single endpoint

---
+++ data-import-services/event-producer/tests/test_main.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_create_event():
+    response = client.post(
+        "/events/",
+        headers={"X-Token": "coneofsilence"},
+        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foobar",
+        "title": "Foo Bar",
+        "description": "The Foo Barters",
+    }
