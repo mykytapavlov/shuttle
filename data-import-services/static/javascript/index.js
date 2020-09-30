c61af6600ed6673b7c9beeb29481f4896b1b799f
Joseph Aveltsev <aveltsev@gmail.com>
2020-09-30 22:11:33 +0300

    creates Sender class with constructor and send methods

--- data-import-services/static/javascript/index.js
+++ data-import-services/static/javascript/index.js
@@ -1,23 +1,23 @@
-class CSVReader extends FileReader{
+class CSVReader extends FileReader {
     /* 
-    On succeful load of file creates JSON object 
+    On successful load of file creates JSON object
     for each line in CSV file and stores it in array:
     this.data = [{"column": "value"}, {"column": "value"}]
     */
-    constructor(){
+    constructor() {
         super();
         this.data = [];
         this.onload = this.handleOnload.bind(this);
     }
 
-    handleOnload(event){
+    handleOnload(event) {
         const text = event.target.result;
         const [firstLine, ...lines] = text.split('\n');
         const columnNames = firstLine.split(',');
         lines.forEach(line => {
             const values = line.split(',');
             let row = {};
-            for (let i = 0; i < columnNames.length; i++){
+            for (let i = 0; i < columnNames.length; i++) {
                 row[columnNames[i]] = values[i];
             }
             this.data.push(JSON.stringify(row));
@@ -26,8 +26,8 @@ class CSVReader extends FileReader{
 }
 
 
-class FileManager{
-    static readFilesCSV(files){
+class FileManager {
+    static readFilesCSV(files) {
         /*
         Reads multiple CSV files and returns dictionary:
         result = {
@@ -36,8 +36,8 @@ class FileManager{
         }
         */
         let result = {};
-        for (let i = 0; i < files.length; i++){
-            (function(file){
+        for (let i = 0; i < files.length; i++) {
+            (function (file) {
                 result[file.name] = []
                 const reader = new CSVReader()
                 reader.readAsText(file, "UTF-8");
@@ -48,6 +48,28 @@ class FileManager{
     }
 }
 
+class Sender {
+    constructor(jsonObjects, limit) {
+        this.jsonObjects = jsonObjects;
+        this.limit = limit;
+    }
+
+    send() {
+        let data = {events: []};
+        for (let i = 0; i < this.limit; i++) {
+            const randomJsonObject = this.jsonObjects[Math.floor(Math.random() * this.jsonObjects.length)];
+            data.events.push(randomJsonObject);
+        }
+
+        fetch("/events", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(r => console.log(r));
+    }
+}
 
 (function main() {
     const fileSelector = document.getElementById('file-selector');
