33e55fc2ef71dbbbb791651101d2069169e263ba
mykytapavlov <mykytapavlov@users.noreply.github.com>
2020-10-03 13:56:07 +0300

    #17: Reworks files upload logic

--- data-import-services/static/javascript/index.js
+++ data-import-services/static/javascript/index.js
@@ -1,59 +1,43 @@
-class CSVReader extends FileReader{
-    /* 
-    On succeful load of file creates JSON object 
-    for each line in CSV file and stores it in array:
-    this.data = [{"column": "value"}, {"column": "value"}]
-    */
-    constructor(){
-        super();
-        this.data = [];
-        this.onload = this.handleOnload.bind(this);
-    }
+const notify = (fn, handler) => e => {
+    fn(handler(e));
+};
 
-    handleOnload(event){
-        const text = event.target.result;
-        const [firstLine, ...lines] = text.split('\n');
-        const columnNames = firstLine.split(',');
-        lines.forEach(line => {
+
+const post = rows => {
+    console.log(rows);  // TODO: POST request instead
+};
+
+
+const parse = e => {
+    const text = e.target.result;
+    const [firstLine, ...lines] = text.split('\n');
+    const columnNames = firstLine.split(',');
+    const data = []
+    lines.forEach(line => {
+        if (line) {
             const values = line.split(',');
             let row = {};
-            for (let i = 0; i < columnNames.length; i++){
+            for (let i = 0; i < columnNames.length; i++) {
                 row[columnNames[i]] = values[i];
-            }
-            this.data.push(JSON.stringify(row));
-        });
-    }
-}
-
-
-class FileManager{
-    static readFilesCSV(files){
-        /*
-        Reads multiple CSV files and returns dictionary:
-        result = {
-            "file_name_1": [{"column": "value"}, {"column": "value"}],
-            "file_name_2": [{"column": "value"}, {"column": "value"}],
-        }
-        */
-        let result = {};
-        for (let i = 0; i < files.length; i++){
-            (function(file){
-                result[file.name] = []
-                const reader = new CSVReader()
-                reader.readAsText(file, "UTF-8");
-                result[file.name] = reader.data;
-            })(files[i]);
-        }
-        return result;
-    }
-}
-
-
-(function main() {
-    const fileSelector = document.getElementById('file-selector');
-    fileSelector.addEventListener('change', (event) => {
-        const files = event.target.files;
-        const result = FileManager.readFilesCSV(files)
-        console.log('Files: ', result)
+            };
+            data.push(JSON.stringify(row));
+        };
     });
-})();
+    return data;
+};
+
+
+const read = e => {
+    const files = e.target.files;
+    for (let i = 0; i < files.length; i++) {
+        ((file) => {
+            const reader = new FileReader();
+            reader.onload = notify(post, parse);
+            reader.readAsText(file, "UTF-8");
+        })(files[i]);
+    };
+};
+
+
+const fileSelector = document.getElementById('file-selector');
+fileSelector.onchange = read;
